9bca3fa2e90108d3d77683a29d7e1ff4be9d5327
Hannah Chea <cheahannah@gmail.com>
2026-05-31 09:05:50 -0700

    Add Person schema, edit home page layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NAME: "HANNAH CHEA",
+  EMAIL: "xirimpi@gmail.com",
+  //NUM_POSTS_ON_HOMEPAGE: 0,
+  //NUM_WORKS_ON_HOMEPAGE: 0,
+  //NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
@@ -27,6 +27,7 @@ export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
 };
+
 
 export const SOCIALS: Socials = [
   { 
